c5f49f1ae08c101dfd7fa9fe74d283f9268a4cdb
bjoernQ <bjoern.quentin@mobile-j.de>
2024-10-24 12:59:23 +0200

    Next

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use clap::Parser;
 struct Args {
     #[arg(short, long)]
     features: Option<String>,
+    #[arg(short, long)]
+    chip: Option<String>,
 }
 
 fn main() {
@@ -23,14 +25,25 @@ fn main() {
         .join(", ");
     let features_flat = features.join(" ");
 
+    let chip = args.chip.unwrap_or("esp32c3".to_string()).to_lowercase();
+
+    let target = match chip.as_str() {
+        "esp32" => "xtensa-esp32-none-elf",
+        "esp32s2" => "xtensa-esp32s2-none-elf",
+        "esp32s3" => "xtensa-esp32s3-none-elf",
+        "esp32c2" | "esp32c3" => "riscv32imc-unknown-none-elf",
+        _ => "riscv32imac-unknown-none-elf",
+    };
+
     let content = format!(
         "
     {{
+        \"rust-analyzer.cargo.target\": \"{target}\",
         \"rust-analyzer.cargo.features\": [
-            {}
+            {features_list}
         ],
         \"rust-analyzer.check.overrideCommand\": [
-            \"cargo\",
+            \"cargo",
             \"check\",
             \"--examples\",
             \"--bins\",
@@ -38,13 +51,15 @@ fn main() {
             \"--message-format=json\",
             \"--no-default-features\",
             \"--features\",
-            \"{}\",
+            \"{features_flat}\",
+            \"--target\",
+            \"{target}\",
         ],
         \"rust-analyzer.check.allTargets\": false,
         \"rust-analyzer.cargo.buildScripts.invocationLocation\": \"root\",
         \"rust-analyzer.check.invocationLocation\": \"root\",
         \"rust-analyzer.cargo.buildScripts.overrideCommand\": [
-            \"cargo\",
+            \"cargo",
             \"check\",
             \"--examples\",
             \"--bins\",
@@ -52,12 +67,13 @@ fn main() {
             \"--message-format=json\",
             \"--no-default-features\",
             \"--features\",
-            \"{}\",
+            \"{features_flat}\",
+            \"--target\",
+            \"{target}\",
         ],
         \"rust-analyzer.showUnlinkedFileNotification\": false,
     }}     
-    ",
-        features_list, features_flat, features_flat
+    "
     );
 
     std::fs::create_dir_all(".vscode/").unwrap();
